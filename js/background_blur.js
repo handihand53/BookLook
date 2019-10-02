dd1a203dc522988472c0e82fb1c683f360495cc8
Handi Hermawan <handihand53@gmail.com>
2019-10-02 11:00:22 +0700

    adding mybook.html + edit user.html

--- js/background_blur.js
+++ js/background_blur.js
@@ -4,6 +4,7 @@ window.onload = function(){
     var drop_content = document.getElementById("drop");
     var shop_drop = document.getElementById("drop-shop");
     var profile = document.getElementById("profile");
+    var btn_profile = document.getElementById("btn_profile");
     
     function fixed(){
       bg.style.position ="fixed";
@@ -13,11 +14,13 @@ window.onload = function(){
       bg.style.position ="unset";
     }
 
+    btn_profile.addEventListener("mouseover", fixed);
     profile.addEventListener("mouseover", fixed);
-    profile.addEventListener("mouseout", unset);
+    kategori_Drop.addEventListener("mouseover", fixed);
     shop_drop.addEventListener("mouseover", fixed);
+    btn_profile.addEventListener("mouseout", unset);
+    profile.addEventListener("mouseout", unset);
     shop_drop.addEventListener("mouseout", unset);
     bg.addEventListener("mouseover", unset);
     drop_content.addEventListener("mouseout", unset);
-    kategori_Drop.addEventListener("mouseover", fixed);
   }
